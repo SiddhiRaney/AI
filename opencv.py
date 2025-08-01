81e25bbbdd7d1fc464e6e92cd4c4561f59a0e0c3
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-08-01 20:24:58 +0530

    Update opencv.py

--- opencv.py
+++ opencv.py
@@ -4,3 +4,13 @@ image = cv2.imread('example.jpg')
 cv2.imshow('Image Window', image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+#resizing image
+import cv2
+
+image = cv2.imread('example.jpg')
+resized_image = cv2.resize(image, (300, 200))  # width=300, height=200
+
+cv2.imshow('Resized Image', resized_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
