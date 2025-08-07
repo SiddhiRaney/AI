db0c8986a56473b210bc1f0dfbd879688f4254f8
Siddhi Raney <164665691+SiddhiRaney@users.noreply.github.com>
2025-08-07 23:19:27 +0530

    Update newspaper.py

--- newspaper.py
+++ newspaper.py
@@ -12,3 +12,35 @@ print("Authors:", article.authors)
 print("Published Date:", article.publish_date)
 print("Summary:", article.summary)
 print("Text:", article.text[:500])  # Only showing first 500 chars
+
+
+from newspaper import Article
+
+url = 'https://www.nytimes.com/2024/04/05/world/europe/france-retirement-age.html'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+print("Title:", article.title)
+print("Authors:", article.authors)
+print("Published Date:", article.publish_date)
+print("Summary:", article.summary)
+print("Text:", article.text[:500])
+
+
+from newspaper import Article
+
+url = 'https://edition.cnn.com/2023/11/03/tech/twitter-x-advertising/index.html'
+article = Article(url)
+
+article.download()
+article.parse()
+article.nlp()
+
+print("Title:", article.title)
+print("Authors:", article.authors)
+print("Published Date:", article.publish_date)
+print("Summary:", article.summary)
+print("Text:", article.text[:500])
